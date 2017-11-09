7050b00b3e633012876873456df6218a21f38ee5
Alejandro Brizo <abrizho_8594@hotmail.com>
2017-11-09 01:28:03 -0600

    Trabajando en Logica de Login.

--- web-project/javascript/controller.js
+++ web-project/javascript/controller.js
@@ -1,37 +1,60 @@
+var userLogged = {
+    name: "AlejandroBrizo",
+    typeUser: "Employee",
+    state: "LogOff"
+}
+
 $(document).ready(function() { 
+
+    verifyAccount();
 
     $("#MenuPermisos li").click(function() {
 
         if($(this).attr('id') == "solicitudPorDia"){
-        loadPageSectionOnMain("SolicitudPorDia"); 
+        loadPageSectionOnMain("#HomeContent", "SolicitudPorDia"); 
         console.log("Hola");         
         }
         if($(this).attr('id') == "solicitudPorDuelo"){
-        loadPageSectionOnMain("SolicitudPorDuelo");          
+        loadPageSectionOnMain("#HomeContent", "SolicitudPorDuelo");          
         }
         if($(this).attr('id') == "solicitudPorIncapacidad"){
-        loadPageSectionOnMain("SolicitudExcusaMedica");          
+        loadPageSectionOnMain("#HomeContent", "SolicitudExcusaMedica");          
         }
         if($(this).attr('id') == "solicitudPorMaternidad"){
-        loadPageSectionOnMain("SolicitudPorMaternidad");          
+        loadPageSectionOnMain("#HomeContent", "SolicitudPorMaternidad");          
         }
         if($(this).attr('id') == "solicitudPorMatrimonio"){
-            loadPageSectionOnMain("SolicitudPorMatrimonio");          
+            loadPageSectionOnMain("#HomeContent", "SolicitudPorMatrimonio");          
         }
         if($(this).attr('id') == "solicitudPorSalida"){
-            loadPageSectionOnMain("SolicitudPorSalida");          
+            loadPageSectionOnMain("#HomeContent", "SolicitudPorSalida");          
         }
         if($(this).attr('id') == "solicitudesExistentes"){
-            loadPageSectionOnMain("SolicitudesExistentes");          
+            loadPageSectionOnMain("#HomeContent", "SolicitudesExistentes");          
         }
 
     });
 
  });
 
+$("#goToRegister").click(function() {
+    goToPage("Register");
+});
 
-//Brizo: Cargar cualquier pagina
-function loadPageSectionOnMain(page){
+$("#btn-SignIn").click(function() {
+    userLogged.state = "Logged";
+    goToPage("Home");
+});
+
+function verifyAccount(){
+    if(userLogged.state == "LogOff"){
+        goToPage("Login");
+    }else if(userLogged.state == "Logged"){
+        goToPage("Home");
+    }
+}
+
+function goToPage(page){
     $.ajax(
         {
             url: "ajax/openFile.php",
@@ -39,7 +62,24 @@ function loadPageSectionOnMain(page){
             method:"POST",
             dataType: "json",
             success:function(response){
-                $("#main-content").html(response[0].content);
+                if(page== "Register"){
+                    $("#body").removeClass("login-page");   
+                    $("#body").addClass("register-page");   
+                }
+                if(page=="Home"){
+                    $("#body").removeClass("login-page");   
+                    $("#body").addClass("skin-black");
+                    $("#body").addClass("sidebar-mini");                       
+                    $("#MasterContent").addClass("wrapper");                                           
+                }
+                if(page =="Login"){
+                    console.log("GoToLogin()");
+                    $("#body").removeClass("sidebar-mini");
+                    $("#body").removeClass("skin-black");
+                    $("#body").addClass("login-page");                       
+                    $("#MasterContent").removeClass("wrapper");   
+                }
+                $("#MasterContent").html(response[0].content);
             },
             error:function(error){
                 alert("No existe la ruta o el archivo. PENDIENTE.");
@@ -48,3 +88,20 @@ function loadPageSectionOnMain(page){
     );
 }
 
+//Brizo: Cargar cualquier pagina, en una Section especifica (mediante id)
+function loadPageSectionOnMain(inSection, page){
+    $.ajax(
+        {
+            url: "ajax/openFile.php",
+            data: "pageName=../views/"+page+".html",
+            method:"POST",
+            dataType: "json",
+            success:function(response){
+                $(inSection).html(response[0].content);
+            },
+            error:function(error){
+                alert("No existe la ruta o el archivo. PENDIENTE.");
+            }
+        }
+    );
+}
